a7281076e79120c055b3a372bd605a2597bc2c47
ayisha <ayisha.hayath.n@gmail.com>
2024-07-28 13:14:24 +0530

    completed

--- script.js
+++ script.js
@@ -12,13 +12,35 @@ addbtn.addEventListener("click",function(){
 //select cancel button
 
 var cancelbtn=document.querySelector("#cancel-popup")
-var addbtn=document.querySelector("#")
 
 cancelbtn.addEventListener("click",function(event){
     event.preventDefault()
     overlay.style.display="none"
     popup.style.display="none"
-addbtn.addEventListener("click",function(event){
+})
+
+//box,add-book,book-title-input,book-author-input,book-description-input
+
+var box=document.querySelector(".box")
+var addbook=document.querySelector("#add-book")
+var booktitle=document.querySelector("#book-title-input")
+var bookauthor=document.querySelector("#book-author-input")
+var description=document.querySelector("#book-description-input")
+var deletebtn=document.querySelector(".btn")
+
+addbook.addEventListener("click",function(event){
     event.preventDefault()
+    var div=document.createElement("div")
+    div.setAttribute("class","book-box")
+    div.innerHTML=`<h2>${booktitle.value}</h2>
+        <h5>${bookauthor.value}</h5>
+        <p>${description.value}</p>`
+    box.append(div)
+    overlay.style.display="none"
+    popup.style.display="none"
 })
-})
+
+
+function deletebook(event){
+    event.target.parentElement.remove()
+}
